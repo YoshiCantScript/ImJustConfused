ab157aef82e2e94a3447999540a6718e46736dce
YoshiCantScript <pietrosilv9@gmail.com>
2024-03-09 19:17:06 +0100

    mainpage

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -10,7 +10,7 @@ import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 
 class ExampleProvider(val plugin: TestPlugin) : MainAPI() { // all providers must be an intstance of MainAPI
     override var mainUrl = "https://calciostreaming.lol"
-    override var name = "Example sex provide"
+    override var name = "Example sex provid"
     override val supportedTypes = setOf(TvType.Live)
 
     override var lang = "it"
